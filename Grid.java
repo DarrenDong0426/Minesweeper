0d3da7482b04f2e4753d4fa11a076f17b4c62c16
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2021-02-24 18:51:08 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 
+public class Grid{
+  
+  private ArrayList<Integer> length;
+  private ArrayList<ArrayList<Integer>> width;
+  private int numberOfMines;
+  
+  public Grid(int l, int w, int m){
+    
+    length = new ArrayList<Integer>(l);
+    width = new ArrayList<ArrayList<Integer>>(w);
+    numberOfMines = m;
+    
+  }
+  
+}
