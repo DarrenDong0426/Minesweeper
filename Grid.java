738c1ceafa79c368c31338edc70d8658de63a9c3
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2021-02-24 22:14:31 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -2,22 +2,36 @@ import java.util.ArrayList;
 
 public class Grid extends Minesweeper{
 	
+	ArrayList<Button> length;
+	ArrayList<ArrayList<Button>> width;
+	
 	public Grid(){}
 	
 	public Grid(int l, int w, int m) {
 		
-		ArrayList<Button> length = new ArrayList<Button>(l);
-		ArrayList<ArrayList<Button>> width = new ArrayList<ArrayList<Button>>(w);
+		width = new ArrayList<ArrayList<Button>>(w);
 		
 		for (int i = 0; i < l; i++){
+			length = new ArrayList<Button>(l);
+			width.add(length);
 			for (int j = 0; j < w; j++){
 				Button button = new Button();
 				getPanel().add((button.getButton()));
+				(width.get(i)).add(button);
 			}				
 		}
-		
-		
-		
+		SetMines(l, w, m);
+	}
+	
+	public void SetMines(int l, int w, int m){
+		while (m > 0){
+			int x = (int)(Math.random() * l);
+			int y = (int)(Math.random() * w);
+			if ((((width.get(y)).get(x)).MineCheck()) == false){
+				((width.get(y)).get(x)).IntoMine();
+				m--;
+			}
+		}		
 	}
 				
 }
