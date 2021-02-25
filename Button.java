ba2efe9ae638773f77f0befdbc783c0157bc7a1e
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2021-02-24 20:48:13 -0500

    Update Button.java

--- Button.java
+++ Button.java
@@ -1,3 +1,37 @@
-public class Button{
-  
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+
+public class Button extends JFrame
+			implements MouseListener{
+
+	private JButton button;
+		
+	public Button(){
+		button = new JButton("Yes");
+		button.addMouseListener(this);
+	}
+	
+	public JButton getButton(){
+		return button;
+	}
+
+	public void mouseClicked(MouseEvent arg0) {}
+
+	public void mouseEntered(MouseEvent arg0) {}
+
+	public void mouseExited(MouseEvent arg0) {}
+
+	public void mousePressed(MouseEvent arg0) {}
+
+	public void mouseReleased(MouseEvent arg0) {
+
+	}
+
+	
 }
