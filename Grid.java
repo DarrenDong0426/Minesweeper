631f7d1b1b61a9ec31ca694c854afc3560b42c4c
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2021-02-25 22:33:32 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,6 +1,9 @@
+
 import java.util.ArrayList;
 
 public class Grid extends Minesweeper{
+	
+	
 	
 	ArrayList<Button> length;
 	ArrayList<ArrayList<Button>> width;
@@ -20,9 +23,11 @@ public class Grid extends Minesweeper{
 				(width.get(i)).add(button);
 			}				
 		}
+		
 		SetMines(l, w, m);
+		SetNumbers(l, w);
 	}
-	
+		
 	public void SetMines(int l, int w, int m){
 		while (m > 0){
 			int x = (int)(Math.random() * l);
@@ -33,5 +38,51 @@ public class Grid extends Minesweeper{
 			}
 		}		
 	}
-				
+	
+	public void SetNumbers(int l, int w){
+		for(int i = 0; i < l; i++){
+			for (int j = 0; j < w; j++){
+				int nums = 0; 
+				if (isInGrid(i - 1, j) == true){
+					if (((width.get(i-1)).get(j)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i - 1, j - 1) == true){
+					if (((width.get(i-1)).get(j-1)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i - 1, j + 1) == true){
+					if (((width.get(i-1)).get(j+1)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i, j - 1) == true){
+					if (((width.get(i)).get(j-1)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i, j + 1) == true){
+					if (((width.get(i)).get(j+1)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i + 1, j) == true){
+					if (((width.get(i+1)).get(j)).MineCheck() == true)
+						nums++;
+				}
+				if (isInGrid(i + 1, j - 1) == true){
+					if (((width.get(i+1)).get(j-1)).MineCheck() == true)
+						nums++;		
+				}
+				if (isInGrid(i + 1, j + 1) == true){
+					if (((width.get(i + 1)).get(j+1)).MineCheck() == true)
+						nums++;	
+				}
+				((width.get(i)).get(j)).IntoNumbers(nums);
+			}
+		}
+	}
+	
+	public boolean isInGrid(int i, int j){
+		if (i < 0 || j < 0 || i > width.size() - 1|| j > length.size() - 1)
+			return false;
+		return true;
+	}
 }
